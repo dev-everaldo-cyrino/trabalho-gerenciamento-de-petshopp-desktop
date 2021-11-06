e80b1761076c86ac841d0a4dbac6b3d9d67c4af8
dev-everaldo-cyrino <alonedev3d2a@gmail.com>
2021-11-06 17:38:10 -0300

    tela de cadastro quase terminada
    finalizado quase toda a tela, falta apenas um frame com um botão de pesquisa para ter acesso ao banco de dados

---
+++ consulta.py
@@ -0,0 +1,45 @@
+from tkinter import *
+
+root = Tk()
+root.geometry('700x500+0+0')
+root.title('consulta')
+
+hora = StringVar()
+
+Label(root,text='tela de consulta',fg='royal blue',font='arial 40 bold').place(x=135,y=0)
+
+
+Label(root,text='nome : ',fg='steelblue',font='arial 18 bold').place(x=15,y=50)
+nomee = Entry(root,width='30')
+nomee.place(x=100,y=60)
+
+
+Label(root,text='data  :',fg='steelblue',font='arial 18 bold').place(x=15,y=100)
+datae = Entry(root,width='30')
+datae.place(x=100,y=110)
+
+
+Label(root,text='horarios :',fg='steelblue',font='arial 18 bold').place(x=15,y=160)
+horario1 = Radiobutton(root,text='7h-9h',value='manha1' ,variable=hora)
+horario1.place(x=10,y=190)
+horario2 = Radiobutton(root,text='9h30m-11hh',value='manha2' ,variable=hora)
+horario2.place(x=80,y=190)
+horario3 = Radiobutton(root,text='13h-15h',value='manha3' ,variable=hora)
+horario3.place(x=10,y=210)
+horario4 = Radiobutton(root,text='15h30m-17h',value='manha4' ,variable=hora)
+horario4.place(x=80,y=210)
+
+
+Label(root,text='motivo :',fg='steelblue',font='arial 18 bold').place(x=15,y=230)
+obs = Text(root,height='5',width='30')
+obs.place(x=15,y=270)
+
+
+btn_cadastrar= Button(root,text='limpar',bg='salmon',font='arial 20 bold',height='3')
+btn_cadastrar.place(x=170,y=365)
+btn_cadastrar= Button(root,text='cadastrar',bg='dodger blue',font='arial 20 bold',width='8')
+btn_cadastrar.place(x=10,y=365)
+btn_cadastrar= Button(root,text='atualizar',bg='royal blue',font='arial 20 bold',width='8')
+btn_cadastrar.place(x=10,y=429)
+
+root.mainloop()
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ root = Tk()
 root.geometry('700x500+0+0')
 root.title('tela principal')
 
-Label(root,text='petshopp nova era',fg='steelblue',font='arial 40 bold').place(x=135,y=15)
+Label(root,text='petshopp nova era',fg='royal blue',font='arial 40 bold').place(x=135,y=15)
 btn_stok= Button(root,text='STOKE',bg='white smoke',font='arial 20 bold',width='34').place(x=50,y=90)
 btn_stok= Button(root,text='CONSULTA',bg='white smoke',font='arial 20 bold',width='34').place(x=50,y=160)
 btn_stok= Button(root,text='VENDER',bg='white smoke',font='arial 20 bold',width='34').place(x=50,y=230)
